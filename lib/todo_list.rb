c1e78ad4f510dd45bacf4cfabcef8e7c1a7b38f0
Juan Martin Villanueva <juan.villanueva@cisb.com.ar>
2025-01-23 13:37:22 -0300

    feat: Agrega ID a la tarea

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -1,27 +1,67 @@
-# lib/todo_list.rb
+require 'json'
+
 class TodoList
-  def initialize
-    @tasks = []
+  def initialize(file_path = 'tasks.json')
+    @file_path = file_path
+    @tasks = load_tasks
+    @next_id = @tasks.empty? ? 1 : @tasks.map { |task| task[:id] }.max + 1
   end
 
   def add_task(description)
-    @tasks << { description: description, completed: false }
+    id = @next_id
+    @tasks << { id: id, description: description, completed: false }
+    @next_id += 1
+    save_tasks
+    id
   end
 
-  def find_task(description)
-    @tasks.find { |task| task[:description] == description }
+  def find_task_by_id(id)
+    @tasks.find { |task| task[:id] == id }
   end
 
   def all_tasks
     @tasks
   end
-  
-  def complete_task(description)
-    task = find_task(description)
-    task[:completed] = true if task
+
+  def complete_task(id)
+    task = find_task_by_id(id)
+    if task
+      task[:completed] = true
+      save_tasks
+    end
   end
-  
-  def delete_task(description)
-    @tasks.reject! { |task| task[:description] == description }
+
+  def delete_task(id)
+    @tasks.reject! { |task| task[:id] == id }
+    save_tasks
+  end
+
+  def edit_task_description(id, new_description)
+    task = find_task_by_id(id)
+    if task
+      task[:description] = new_description
+      task[:completed] = false
+      save_tasks
+    end
+  end
+
+  def completed_tasks
+    @tasks.select { |task| task[:completed] }
+  end
+
+  private
+
+  def load_tasks
+    if File.exist?(@file_path)
+      JSON.parse(File.read(@file_path), symbolize_names: true)
+    else
+      []
+    end
+  end
+
+  def save_tasks
+    File.open(@file_path, 'w') do |file|
+      file.write(JSON.pretty_generate(@tasks))
+    end
   end
 end
--- spec/todo_list_spec.rb
+++ spec/todo_list_spec.rb
@@ -1,38 +1,61 @@
-# spec/todo_list_spec.rb
 require 'rspec'
+require 'fileutils'
 require_relative '../lib/todo_list'
 
+# Define file_path fuera del bloque describe
+file_path = 'spec/test_tasks.json'
+
 RSpec.describe TodoList do
+  let(:todo_list) { TodoList.new(file_path) }
+
+  before(:each) { FileUtils.rm_f(file_path) }
+
   it 'agrega una nueva tarea' do
-    todo_list = TodoList.new
-    todo_list.add_task('Aprender TDD')
-    task = todo_list.find_task('Aprender TDD')
+    id = todo_list.add_task('Aprender TDD')
+    task = todo_list.find_task_by_id(id)
     expect(task).not_to be_nil
     expect(task[:description]).to eq('Aprender TDD')
     expect(task[:completed]).to be false
   end
 
   it 'lista todas las tareas' do
-    todo_list = TodoList.new
     todo_list.add_task('Aprender TDD')
     todo_list.add_task('Escribir código')
     tasks = todo_list.all_tasks
     expect(tasks.size).to eq(2)
   end
-  
+
   it 'marca una tarea como completada' do
-    todo_list = TodoList.new
-    todo_list.add_task('Aprender TDD')
-    todo_list.complete_task('Aprender TDD')
-    task = todo_list.find_task('Aprender TDD')
+    id = todo_list.add_task('Aprender TDD')
+    todo_list.complete_task(id)
+    task = todo_list.find_task_by_id(id)
     expect(task[:completed]).to be true
   end
-  
-  it 'elimina una tarea por descripción' do
-    todo_list = TodoList.new
-    todo_list.add_task('Aprender TDD')
-    todo_list.delete_task('Aprender TDD')
-    task = todo_list.find_task('Aprender TDD')
+
+  it 'elimina una tarea por ID' do
+    id = todo_list.add_task('Aprender TDD')
+    todo_list.delete_task(id)
+    task = todo_list.find_task_by_id(id)
     expect(task).to be_nil
   end
+
+  it 'edita la descripción de una tarea y la marca como pendiente' do
+    id = todo_list.add_task('Aprender TDD')
+    todo_list.edit_task_description(id, 'Aprender TDD con RSpec')
+    task = todo_list.find_task_by_id(id)
+    expect(task).not_to be_nil
+    expect(task[:description]).to eq('Aprender TDD con RSpec')
+    expect(task[:completed]).to be false
+  end
+
+  it 'lista solo las tareas completadas' do
+    todo_list.add_task('Aprender TDD')
+    id = todo_list.add_task('Escribir código')
+    todo_list.complete_task(id)
+    completed_tasks = todo_list.completed_tasks
+    expect(completed_tasks.size).to eq(1)
+    expect(completed_tasks.first[:description]).to eq('Escribir código')
+  end
+
+  after(:context) { FileUtils.rm_f(file_path) }
 end
